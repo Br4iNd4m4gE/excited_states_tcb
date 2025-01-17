7c028178613fd154fefd7b9aff5858747c3ac55d
Christian Schmidt <christian.schmidt@kit.edu>
2025-01-17 06:06:11 +0100

    removed comments

--- nn_scripts/nn_spectrum.py
+++ nn_scripts/nn_spectrum.py
@@ -178,12 +178,6 @@ hp_model = tuner.hypermodel.build(best_hps)
 
 ## 3. Training best hp model
 
-# # Set normalization layer to the inverse distances
-# # The layer calculating inverse distances ist the first layer of the model. The result of this layer is used to fit the normalization layer (x -> x-µ/std).
-# # Basically a normalization layer is fitted to the inverse distances.
-# feat_precomp = precompute_feature_in_chunks(data["coords_scaled"], hp_model, batch_size=32)  # calculates the inverse distances (features = output of first layer)
-# set_const_normalization_from_features(feat_precomp, hp_model) # set the normalization layer to the inverse distances
-
 # fit the models
 hp_hist = hp_model.fit(x_train, target, epochs=epochs, validation_split=0.1, verbose=2, callbacks=callbacks)
 
